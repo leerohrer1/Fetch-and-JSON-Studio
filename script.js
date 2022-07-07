90ffb60232f669d5c26bab5188878834155bb9d4
leerohrer1 <loginlee3@gmail.com>
2022-07-06 20:32:35 -0500

    Finished assignment

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-// TODO: add code here
+window.addEventListener("load", function() {
+  fetch("https://handlers.education.launchcode.org/static/astronauts.json")
+  .then(response => response.json())
+  .then(json => {
+    const astro = document.getElementById("container");
+    let block = '';
+    for (let i = 0; i < json.length; i++) {
+    block += `
+    <div class="astronaut">
+      <div class="bio">
+        <h3>${json[i].firstName} ${json[i].lastName}</h3>
+        <ul>
+          <li>Hours in space: ${json[i].hoursInSpace}</li>
+          <li>Active: ${json[i].active}</li>
+          <li>Skills: ${json[i].skills}</li>
+        </ul>
+
+    </div>
+    <img class="avatar" src="${json[i].picture}">
+
+    </div>
+    `
+  }
+  astro.innerHTML = block;
+  
+})
+
+})
